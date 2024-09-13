0221c5e5095f53c07004005edbb5d21e18a788d6
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-13 20:00:02 +0000

    chore: add 'Valid Anagram'

---
+++ problems/242. Valid Anagram.py
@@ -0,0 +1,16 @@
+from collections import defaultdict
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        d = defaultdict(lambda: 0)
+        for c in s:
+            d[c] += 1
+
+        for c in t:
+            if d[c] == 0:
+                return False
+            d[c] -= 1
+        return True
