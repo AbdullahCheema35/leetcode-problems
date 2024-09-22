e42046d0b24b26172eee9832a6e37b9b18a90059
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-22 22:35:03 +0000

    chore: add another implementation for 'ransomNote'

--- problems/383. Ransom Note.py
+++ problems/383. Ransom Note.py
@@ -1,15 +1,24 @@
-from collections import defaultdict
+# from collections import defaultdict
+
+
+# class Solution:
+#     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+#         d = defaultdict(lambda: 0)
+#         for letter in magazine:
+#             d[letter] += 1
+
+#         for letter in ransomNote:
+#             if d[letter] < 1:
+#                 return False
+#             d[letter] -= 1
+
+#         return True
 
 
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        d = defaultdict(lambda: 0)
-        for letter in magazine:
-            d[letter] += 1
-
-        for letter in ransomNote:
-            if d[letter] < 1:
+        for letter in set(ransomNote):
+            if magazine.count(letter) < ransomNote.count(letter):
                 return False
-            d[letter] -= 1
 
         return True
