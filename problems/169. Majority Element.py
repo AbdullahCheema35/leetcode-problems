708e2e423451fbf7b91dffb87b7f43b4eee340bb
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-29 21:45:34 +0000

    chore: add 'Majority Element'

---
+++ problems/169. Majority Element.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        num = nums[0]
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[i] == num:
+                count += 1
+            else:
+                if count == 1:
+                    num = nums[i]
+                else:
+                    count -= 1
+        return num
