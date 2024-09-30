bf6c75cf47804278cefabbac1eef397e03e22a38
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-30 22:46:55 +0000

    fix: non-decreasing order

--- problems/977. Squares of a Sorted Array.py
+++ problems/977. Squares of a Sorted Array.py
@@ -6,11 +6,11 @@ class Solution:
         start: int = 0
         end: int = len(nums) - 1
         result: List[int] = [0] * len(nums)
-        for i, _ in enumerate(result):
+        for i in range(len(nums) - 1, -1, -1):
             if abs(nums[start]) > abs(nums[end]):
-                result[i] = nums[end] ** 2
-                end -= 1
-            else:
                 result[i] = nums[start] ** 2
                 start += 1
+            else:
+                result[i] = nums[end] ** 2
+                end -= 1
         return result
