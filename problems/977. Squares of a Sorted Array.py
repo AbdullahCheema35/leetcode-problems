b38df95d6cff55e81846147210ff5a359992d36b
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-09-30 22:37:26 +0000

    fix: improved SSA using two-pointer approach

--- problems/977. Squares of a Sorted Array.py
+++ problems/977. Squares of a Sorted Array.py
@@ -3,7 +3,14 @@ from typing import List
 
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        for i in range(len(nums)):
-            nums[i] = nums[i] ** 2
-        nums.sort()
-        return nums
+        start: int = 0
+        end: int = len(nums) - 1
+        result: List[int] = [0] * len(nums)
+        for i, _ in enumerate(result):
+            if abs(nums[start]) < abs(nums[end]):
+                result[i] = nums[end] ** 2
+                end -= 1
+            else:
+                result[i] = nums[start] ** 2
+                start += 1
+        return result
