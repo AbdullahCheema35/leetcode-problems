569398d09f25485cbc3fc143c69f8f9593f82588
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-10-01 23:59:51 +0000

    fix: add type checking

--- problems/234. Palindrome Linked List.py
+++ problems/234. Palindrome Linked List.py
@@ -13,12 +13,17 @@ class Solution:
         slow, fast, prev = head, head, None
         while fast and fast.next:
             fast = fast.next.next
-            prev, prev.next, slow = slow, prev, slow.next
+            prev, cast(ListNode, prev).next, slow = (
+                slow,
+                prev,
+                cast(ListNode, slow).next,
+            )
 
         if fast:
             slow = cast(ListNode, slow).next
 
         while prev:
+            assert isinstance(slow, ListNode)
             if slow.val != prev.val:
                 return False
             slow, prev = slow.next, prev.next
