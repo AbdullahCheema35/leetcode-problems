68b17365f8269a9ccb73e7699104a25923bef41d
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-10-02 12:12:16 +0000

    fix: intelligent naive implementation of 'Lowest Common Ancestor of a BST'

--- problems/lca.py
+++ problems/lca.py
@@ -1,6 +1,7 @@
-from typing import Optional
+from typing import Optional, cast
 
 
+# Definition for a binary tree node.
 class TreeNode:
     def __init__(self, x):
         self.val = x
@@ -8,56 +9,21 @@ class TreeNode:
         self.right = None
 
 
-# Definition for a binary tree node.
 class Solution:
-    def __init__(self):
-        self.pFound: bool = False
-        self.qFound: bool = False
-
     def lowestCommonAncestor(
         self, root: "TreeNode", p: "TreeNode", q: "TreeNode"
     ) -> "TreeNode":
         def findLCA(
-            node: Optional[TreeNode], p: TreeNode, q: TreeNode
-        ) -> Optional[TreeNode]:
+            node: Optional["TreeNode"], p: "TreeNode", q: "TreeNode"
+        ) -> Optional["TreeNode"]:
             if not node:
-                return None
-            if node == p:
-                self.pFound = True
-                if self.qFound:
-                    return None
-            if node == q:
-                self.qFound = True
-                if self.pFound:
-                    return None
-
-            left: Optional[TreeNode] = findLCA(node.left, p, q)
-            if not left and (self.pFound and self.qFound):
+                return
+            if node.val in (p.val, q.val):
                 return node
-            right: Optional[TreeNode] = findLCA(node.right, p, q)
-            if not right and (self.pFound and self.qFound):
+            left: Optional["TreeNode"] = findLCA(node.left, p, q)
+            right: Optional["TreeNode"] = findLCA(node.right, p, q)
+            if left and right:
                 return node
-            elif left or right:
-                return left or right
-            return None
+            return left or right
 
-        return findLCA(root, p, q)
-
-    # def findLCA(
-    #     self, node: Optional[TreeNode], p: TreeNode, q: TreeNode
-    # ) -> Optional[TreeNode]:
-    #     if not node:
-    #         return None
-    #     if node == p:
-    #         pFound = True
-    #         return node
-    #     if node == q:
-    #         qFound = True
-    #         return node
-
-    #     left = findLCA(node.left)
-    #     right = findLCA(node.right)
-
-    #     if left and right:
-    #         return node
-    #     return left or right
+        return cast(TreeNode, findLCA(root, p, q))
