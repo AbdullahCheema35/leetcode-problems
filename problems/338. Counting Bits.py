c64c9ec6799a4984f7ae76b13d1a75b830099a55
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-10-11 00:04:46 +0000

    fix: index out of range

--- problems/338. Counting Bits.py
+++ problems/338. Counting Bits.py
@@ -5,16 +5,18 @@ class Solution:
     def countBits(self, n: int) -> List[int]:
         LSB: Final[int] = 1
         ANS: Final[List[int]] = [0] * (n + 1)
-        ANS[1] = 1
         current_MSB: int = 1
 
-        for i in range(2, n + 1):
+        for i in range(1, n + 1):
             if LSB & i:
                 ANS[i] = ANS[i - 1] + 1
-            elif not i & i - 1:
+            elif not i & (i - 1):
                 ANS[i] = 1
                 current_MSB <<= 1
             else:
-                ANS[i] = ANS[(current_MSB - 1) & i] + 1
+                set_bits_except_msb: int = i & (
+                    current_MSB - 1
+                )  # Get the set bits (number) except the MSB
+                ANS[i] = ANS[set_bits_except_msb] + 1
 
         return ANS
