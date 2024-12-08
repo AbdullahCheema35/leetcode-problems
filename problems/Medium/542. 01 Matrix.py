ee7a2dea6cc63596c99c67eaa02d023664f20a5b
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2024-12-08 22:45:12 +0000

    fix: initial implementation for '01 Matrix'

---
+++ problems/Medium/542. 01 Matrix.py
@@ -0,0 +1,50 @@
+import copy
+from typing import List
+
+
+class Solution:
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        n: int = len(mat)  # rows
+        m: int = len(mat[0])  # cols
+
+        MAX_INT = m * n + 1
+
+        dp: List[List[int]] = [[-1] * m for _ in range(n)]
+
+        count: int = 0
+
+        def calculate(
+            mat: List[List[int]], dp: List[List[int]], i: int, j: int, m: int, n: int
+        ) -> int:
+            if i >= n or j >= m:
+                return MAX_INT
+            if dp[i][j] != -1:
+                return dp[i][j]
+            if mat[i][j] == 0:
+                dp[i][j] = 0
+
+            off_by_one_dist: int = min(
+                calculate(mat, dp, i, j + 1, m, n),
+                calculate(mat, dp, i + 1, j, m, n),
+                dp[i - 1][j] if i > 0 and dp[i - 1][j] != -1 else MAX_INT,
+                dp[i][j - 1] if j > 0 and dp[i][j - 1] != -1 else MAX_INT,
+            )
+
+            nonlocal count
+            count += 1
+
+            dp[i][j] = (off_by_one_dist + 1) if mat[i][j] != 0 else 0
+            return dp[i][j]
+
+        calculate(mat, dp, 0, 0, m, n)
+
+        print(count)
+
+        return dp
+
+
+if __name__ == "__main__":
+    input_mat = [[0] * 3 for _ in range(3)]
+    input_mat[1][1] = 1
+    sol = Solution()
+    sol.updateMatrix(input_mat)
