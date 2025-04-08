4983bbd0716d91509d2a73d55620901ae9d19e20
Muhammad Abdullah Cheema <abdullah.cheema@zones.com>
2025-04-08 09:53:44 +0500

    fix: simpler implementation of LCA

--- problems/Medium/236. Lowest Common Ancestor of a Binary Tree.py
+++ problems/Medium/236. Lowest Common Ancestor of a Binary Tree.py
@@ -9,29 +9,19 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        (node, _) = self.find(root, p.val, q.val, 0)
+        node = self.find(root, p, q)
         assert node is not None, "node cannot be None"
         return node
     
-    def find(self, node: Optional['TreeNode'], p: int, q: int, found: int) -> Tuple[Optional['TreeNode'], bool]:
-        if node is None:
-            return None, False
-        itself: bool = node.val == p or node.val == q
-        found = found + 1 if itself else found
-        if itself and found == 2:
-            return node, True
-
-        (left, finshed) = self.find(node.left, p, q, found)
-        if finshed:
-            return (node, True) if itself else (left, True)
-        (right, finshed) = self.find(node.right, p, q, found)
-        if finshed:
-            return (node, True) if itself else (right, True)
+    def find(self, node: Optional['TreeNode'], p: 'TreeNode', q: 'TreeNode') -> Optional['TreeNode']:
+        if node is None or node == p or node == q:
+            return node
         
-        finshed = True if ((left and right) or (itself and (left or right))) else False
+        left = self.find(node.left, p, q)
+        right = self.find(node.right, p, q)
+
+        if left and right:
+            return node
         
-        ret: Optional[TreeNode] = left or right or None
-
-        return (node, finshed) if (itself or (left and right)) else (ret, False)
-            
-
+        return left or right
+    
