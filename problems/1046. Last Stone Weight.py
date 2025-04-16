13414cb3d601a398f97a4b4fc25d1c6a1d39721d
Muhammad Abdullah Cheema <abdullah.cheema@zones.com>
2025-04-16 21:41:34 +0500

    fix: simplify Last Stone Weight

--- problems/1046. Last Stone Weight.py
+++ problems/1046. Last Stone Weight.py
@@ -8,10 +8,10 @@ class Solution:
         heapq.heapify(stones)
 
         while len(stones) > 1:
-            first = -heapq.heappop(stones)
-            second = -heapq.heappop(stones)
+            first = heapq.heappop(stones)
+            second = heapq.heappop(stones)
 
             if first != second:
-                heapq.heappush(stones, -(first-second))
+                heapq.heappush(stones, first-second)
         
         return -stones[0] if len(stones) == 1 else 0
