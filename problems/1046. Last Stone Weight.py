78ef91ca87c07423673a4d53ab67fd0f5a77ffdf
Muhammad Abdullah Cheema <abdullah.cheema@zones.com>
2025-04-16 21:40:52 +0500

    chore: add Last Stone Weight

---
+++ problems/1046. Last Stone Weight.py
@@ -0,0 +1,17 @@
+from typing import List
+import heapq
+
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            first = -heapq.heappop(stones)
+            second = -heapq.heappop(stones)
+
+            if first != second:
+                heapq.heappush(stones, -(first-second))
+        
+        return -stones[0] if len(stones) == 1 else 0
