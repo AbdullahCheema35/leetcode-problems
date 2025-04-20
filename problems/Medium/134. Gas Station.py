3930a797b75d7b1eec52a0dd3f81e9bede6bd448
Muhammad Abdullah Cheema <abdullah.cheema@zones.com>
2025-04-20 16:46:11 +0500

    improve: Gas Station

--- problems/Medium/134. Gas Station.py
+++ problems/Medium/134. Gas Station.py
@@ -4,29 +4,18 @@ from typing import List
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
         N: int = len(gas)
-        diff: List[int] = [0] * N
+        curr: int = 0
+        total: int = 0
 
-        total_sum: int = 0
+        start_index: int = 0
 
         for i in range(N):
-            diff[i] = gas[i] - cost[i]
-            total_sum += diff[i]
-        
-        if total_sum < 0:
-            return -1
-        
-        starting_index: int = 0
-        i: int = 0
-        
-        while i < N:
-            while diff[i] < 0:
-                i += 1
-            
-            curr_sum: int = 0
-            starting_index = i
+            diff: int = gas[i] - cost[i]
+            curr += diff
+            total += diff
 
-            while curr_sum >= 0 and i < N:
-                curr_sum += diff[i]
-                i += 1
-            
-        return starting_index
+            if curr < 0:
+                start_index = i + 1
+                curr = 0
+
+        return start_index if (total >= 0) else -1 
