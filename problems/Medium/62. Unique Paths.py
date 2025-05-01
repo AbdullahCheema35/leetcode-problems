f859a8ed1d7852709a4e35a84a4d0093c233b2fd
Muhammad Abdullah Cheema <muhammadabdullah2510@gmail.com>
2025-05-02 03:30:43 +0500

    chore: add Unique Paths (bottom-up DP) solution

--- problems/Medium/62. Unique Paths.py
+++ problems/Medium/62. Unique Paths.py
@@ -3,17 +3,12 @@ from typing import List
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        def recursion(row: int, col: int, m: int, n: int, dp: List[List[int]]) -> int:
-            print(row, col)
-            if dp[row][col] != 0:
-                return dp[row][col]
-            if row == m - 1 or col == n - 1:  # We are in the last row or the last col
-                dp[row][col] = 1
-            else:
-                dp[row][col] = recursion(row + 1, col, m, n, dp) + recursion(
-                    row, col + 1, m, n, dp
-                )
-            return dp[row][col]
+        above_row: List[int] = [1] * n
 
-        dp: List[List[int]] = [[0] * n for _ in range(m)]
-        return recursion(0, 0, m, n, dp)
+        for _ in range(m - 1):
+            curr_row: List[int] = [1] * n
+            for i in range(1, n):
+                curr_row[i] = curr_row[i - 1] + above_row[i]
+            above_row = curr_row
+
+        return above_row[-1]
